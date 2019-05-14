a327fd994480601f8739bd8fca1ace48cba218d0
Bruno Arruda <arrudabruno10@gmail.com>
2019-05-14 17:18:44 -0300

    Basic user case written

--- sistemas/stefano81-dcpabe/src/main/java/com/brunoarruda/pgc/UseCases.java
+++ sistemas/stefano81-dcpabe/src/main/java/com/brunoarruda/pgc/UseCases.java
@@ -1,28 +1,50 @@
 package com.brunoarruda.pgc;
 
 import java.io.File;
+import java.util.Arrays;
 
 import sg.edu.ntu.sce.sands.crypto.DCPABETool;
 
 public class UseCases {
 
 	final static String DATA_PATH = "data";
-	static String[][] commands = null;
+	final static String TEST_PATH = DATA_PATH + File.separator + "test";
+	
 	public static void main(String[] args) {
-
 		testBasicUserCreation();
 	}
 
 	private static void testBasicUserCreation() {
-		File path = new File(DATA_PATH);
+		File path = new File(TEST_PATH);
 		path.mkdir();
-		String[] commands = {
-			"gsetup" + DATA_PATH + File.separator + "globalSetup",
-			// other commands here
+		String authorityName = "CertifyingAuthority";		
+		String pacientName = "Bob";
+		String fileName = "medicalRecording.txt";
+		
+		String globalSetupPath = TEST_PATH + File.separator + "globalSetup";
+		String authoritySecretKeyPath = TEST_PATH + File.separator + authorityName + "SecretKey";
+		String authorityPublicKeyPath = TEST_PATH + File.separator + authorityName + "PublicKey";
+		String pacientKeyPath = TEST_PATH + File.separator + "Key" + pacientName;
+		String filePath = DATA_PATH + File.separator + fileName ;
+		String encryptedFilePath = TEST_PATH + File.separator + "enc_" + fileName;
+		String decryptedFilePath = TEST_PATH + File.separator + "dec_" + fileName;
+		
+		String[][] commands = {
+				{"gsetup", globalSetupPath},
+				{"asetup", authorityName,  globalSetupPath, authoritySecretKeyPath, authorityPublicKeyPath,
+					"medico", "dono_do_prontuario", "paciente"},
+				{"keyGen", pacientName, "dono_do_prontuario", 
+					globalSetupPath, authoritySecretKeyPath, pacientKeyPath },
+				{"enc", filePath, "dono_do_prontuario OR medico", encryptedFilePath, 
+						globalSetupPath, authorityPublicKeyPath},
+				{"dec", pacientName, encryptedFilePath, decryptedFilePath, globalSetupPath, 
+							pacientKeyPath}
 		};
 
-		// TODO: call main method of DCPABETool with commands, spliting the each string into an array.
-
+		for (String[] args : commands) {
+			System.out.println(String.join(" ", args));
+			DCPABETool.main(args);
+		}
 	}
 
 	private static void testBasicUserEncrypt() {
